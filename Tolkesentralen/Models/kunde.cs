4d6e2400a30627aba9c0aee591f8e64a09461fe3
lunga <s929577@stud.hioa.no>
2017-01-30 16:58:01 +0100

    changes

---
+++ Tolkesentralen/Models/kunde.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tolkesentralen.Models
+{
+    public class kunde
+    {
+
+        public string etternavn { get; set; }
+
+        public string fornavn { get; set; }
+    }
+}
